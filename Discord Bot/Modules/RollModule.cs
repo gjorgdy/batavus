785893287d303d80d284d19147d4e17784e8400a
Jordy <jordyreins@gmail.com>
2025-06-30 17:26:05 +0200

    Implemented basic roll functionality

--- Discord Bot/Modules/RollModule.cs
+++ Discord Bot/Modules/RollModule.cs
@@ -1,24 +1,26 @@
-﻿using Core.Services;
-using Discord;
+﻿using Discord;
 using Discord.Interactions;
+using JetBrains.Annotations;
 using Logic.Roll;
 
 namespace Discord_Bot.Modules;
 
+[UsedImplicitly] // This attribute is used to indicate that this method is used implicitly by the Discord library.
 public class RollModule : InteractionModuleBase
 {
 
     [CommandContextType(InteractionContextType.Guild, InteractionContextType.BotDm, InteractionContextType.PrivateChannel)]
     [IntegrationType(ApplicationIntegrationType.UserInstall, ApplicationIntegrationType.GuildInstall)]
     [SlashCommand("roll", "Rolls a dice.")]
+    [UsedImplicitly] // This attribute is used to indicate that this method is used implicitly by the Discord library.
     public async Task Roll([Summary("input", "The dice to roll, example; 2d4")] string input)
     {
-        var response = new RollCommand(input)
+        var response = await new RollCommand(input)
             .Execute();
 
         var embed = new EmbedBuilder()
             .WithTitle("Rolling " + input)
-            .WithDescription($"> {string.Join(" ", response.Result.Components)}\n**= {response.Result.Total}**")
+            .WithDescription($"> {string.Join(" + ", response.Components)}\n**= {response.Total}**")
             .WithColor(Color.Blue)
             .Build();
 
---
+++ Logic/Roll/Models/FlippableCoin.cs
@@ -0,0 +1,31 @@
+﻿namespace Logic.Roll.Models;
+
+using Modifier = IRollable.Modifier;
+
+public class FlippableCoin : RollableDice
+{
+    public new string InputString => $"${Amount}c" + IRollable.ModifierSuffix(Mod);
+
+    private FlippableCoin(int amount, Modifier mod) : base(amount, 2, mod) {}
+
+    public new static IRollable FromString(string diceString)
+    {
+        // Has 'kh' or 'kl' for keep highest or lowest
+        var mod = IRollable.FilterModifier(diceString);
+        // Remove the modifier from the string if it exists
+        if (mod != Modifier.None)
+        {
+            diceString = diceString[..^2];
+        }
+        // Split the string into amount and sides
+        string[] digits = diceString.Split("d");
+        bool bAmount = int.TryParse(digits[0], out int amount);
+        // Throw an exception if the format is invalid
+        if (!bAmount || amount <= 0)
+        {
+            throw new ArgumentException("Invalid coin format. Use format like '2c', '1ckh', or '3ckl'.");
+        }
+        return new FlippableCoin(amount, mod);
+
+    }
+}
