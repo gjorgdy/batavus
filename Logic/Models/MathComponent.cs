4ae7161f2f93a319bde459dcf855c19025054421
Jordy <jordyreins@gmail.com>
2025-07-06 15:56:24 +0200

    Roll IMP: made mathComponent a generic model

--- Logic/Models/MathComponent.cs
+++ Logic/Models/MathComponent.cs
@@ -1,6 +1,39 @@
-﻿namespace Logic.Models;
+﻿namespace Logic.Roll.Models;
 
-public interface IMathComponent
+public class MathComponent(MathComponent.Type type)
 {
-    string Value { get; }
+    public enum Type
+    {
+        Value = 0,
+        Addition = 1,
+        Subtraction = 2,
+    }
+
+    public static MathComponent ValueFromString(string input)
+    {
+        var c = new MathComponent(Type.Value);
+        if (int.TryParse(input, out int value))
+        {
+            c.Result = new ValueModel(value, input);
+        }
+        else
+        {
+            throw new ArgumentException($"Invalid value '{input}' for MathComponent.");
+        }
+        return c;
+    }
+
+    public ValueModel? Result { get; protected set; }
+    public Type ComponentType { get; init; } = type;
+
+    public override string ToString()
+    {
+        return ComponentType switch
+        {
+            Type.Value => Result?.String ?? "?",
+            Type.Addition => "+",
+            Type.Subtraction => "-",
+            _ => throw new ArgumentOutOfRangeException()
+        };
+    }
 }
